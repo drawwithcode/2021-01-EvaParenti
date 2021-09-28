93f4945d536739389da872b88cea5248f95823f8
EvaParenti <48416326+EvaParenti@users.noreply.github.com>
2021-09-28 21:10:12 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,32 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  angleMode(DEGREES);
+
+  background("pink");
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  noFill();
+
+  let myColor = lerpColor(color("#8A2BE2"), color("#FF8C00"), frameCount / 180); //provide the functions you use (lerpcolor)
+
+  stroke(myColor); //use the variable
+
+  translate(width / 2, height / 2);
+  rotate(frameCount * 3);
+
+  rect(45, 100, 10);
+  rect(90, 12, 200);
+
+  if (frameCount === 120) {
+    noLoop();
+  }
+
+  translate(width / 4, height / 4);
+  rotate(frameCount * 3);
+  rect(10, 10, 10);
+
+  if (frameCount === 120) {
+    noLoop();
+  }
 }
